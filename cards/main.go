8d0a3ba426c2eaa6a2cd30458fb9add4e24c55ac
octavio telles teichner <ott.html@gmail.com>
2022-07-16 04:27:26 +0000

    nested structs

--- cards/main.go
+++ cards/main.go
@@ -1,6 +1,10 @@
 package main
 
+import "fmt"
+
 func main() {
+	fmt.Println("Generating new deck, shuffling and saving to file")
+
 	cards := NewDeck()
 	cards.SaveToFile("my_cards.txt")
 
--- structs/main.go
+++ structs/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type contactInfo struct {
 	email   string
 	zipCode int
@@ -8,8 +10,18 @@ type contactInfo struct {
 type person struct {
 	firstName string
 	lastName  string
+	contactInfo
 }
 
 func main() {
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contactInfo: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 94000,
+		},
+	}
 
+	fmt.Printf("%+v\n", jim)
 }
