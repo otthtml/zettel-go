674a2a679a7eca2a823fde713101c778001986fc
octavio telles teichner <ott.html@gmail.com>
2022-07-18 00:45:47 +0000

    finalize maps

--- maps/main.go
+++ maps/main.go
@@ -2,38 +2,15 @@ package main
 
 import "fmt"
 
-type contactInfo struct {
-	email   string
-	zipCode int
-}
-
-type person struct {
-	firstName string
-	lastName  string
-	contactInfo
-}
-
 func main() {
-	jim := person{
-		firstName: "Jim",
-		lastName:  "Party",
-		contactInfo: contactInfo{
-			email:   "jim@gmail.com",
-			zipCode: 94000,
-		},
+	colors := make(map[string]string)
+	colors["red"] = "#ff0000"
+
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for ", color, "is", hex)
 	}
-
-	(&jim).updateName("Jimmy")
-	p := &jim
-	fmt.Println(p)
-	fmt.Println(&p)
-	// jim.print()
-}
-
-func (p *person) updateName(newName string) {
-	(*p).firstName = newName
-}
-
-func (p person) print() {
-	fmt.Printf("%+v\n", p)
 }
